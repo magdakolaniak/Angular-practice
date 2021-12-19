fc83a42a518e8788d5e159591f7da46145e3778e
magdakolaniak <magda.kolaniak@gmail.com>
2021-12-19 13:48:40 +0100

    Update app.js

--- assignement1/assignment1-starter-code/app.js
+++ assignement1/assignment1-starter-code/app.js
@@ -13,14 +13,16 @@
       $scope.howMany = function () {
         var input = $scope.items;
         $scope.display = input.split(',').length;
+        console.log('Here is input thing', $scope.items.length);
         console.log(input.split(','));
 
-        if ($scope.display > 1 && $scope.display <= 3) {
+        if ($scope.items.length === 0) {
+          $scope.message = 'Please add something';
+        } else if ($scope.display >= 1 && $scope.display <= 3) {
           $scope.message = 'Enjoy!';
         } else if ($scope.display >= 4) {
           $scope.message = "That's too much";
-        } else $scope.message = 'Please add something';
+        }
       };
     });
 })();
-
